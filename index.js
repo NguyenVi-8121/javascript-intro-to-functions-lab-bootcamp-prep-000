f75af35918209d3c1cf42e4dbe763f79b5a9c137
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-26 23:33:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log('logShout'.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase)
+}
+function sayHiToGrandma(string){
+  if (string() === uppercase) {
+  console.log("YES INDEED!")
+}else if (string() === "I love you, Grandma."){
+  console.log("I love you, too.")
+}else{
+  console.log("I can't hear you!")
+}
